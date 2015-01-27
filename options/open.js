af81d6ff04bd60ce221e994052715367c4c9126e
Miguel Frias <isc.miguelfrias@gmail.com>
2015-01-27 00:59:15 -0600

    Remove duplicate grunt tasks

--- options/open.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (grunt) {
-  grunt.config('open', {
-    server: {
-      url: 'http://' + grunt.config.ip + ':<%= connect.options.port %>'
-    }
-  });
-};
