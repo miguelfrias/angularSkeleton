74e84b843f7f7d926b9243001dc9cbf78e403317
Miguel Frias <isc.miguelfrias@gmail.com>
2015-01-27 18:51:30 -0600

    Fixed grunt task: concat

--- grunt/options/concat.js
+++ grunt/options/concat.js
@@ -1,17 +1,24 @@
 'use strict';
 
 module.exports = function (grunt) {
-
   grunt.config('concat', {
-    options: {
-      banner: '// This file is created on '+ new Date().toString() +'. Any change here will be lost.\n\n',
-      separator: ';'
-    },
+      options: {
+          banner: '// This file was created on <%= grunt.template.today("yyyy-mm-dd") %>. Any change here will be lost.\n\n',
+          separator: ';'
+      },
 
-    dist: {
-      src: '<%= grunt.config.app %>/scripts/{,**/**/}*.js',
-      dest: '<%= grunt.config.dist %>/scripts/scripts-concat.js'
-    }
+      dist: {
+        files: {
+          '<%= grunt.config.dist %>/scripts/scripts.min.js': ['<%= grunt.config.app %>/scripts/{,**/**/}*.js'],
+          '<%= grunt.config.dist %>/scripts/scripts.bower.min.js': [
+            '<%= grunt.config.app %>/bower_components/angular/angular.min.js',
+            '<%= grunt.config.app %>/bower_components/angular-route/angular-route.min.js',
+            '<%= grunt.config.app %>/bower_components/angular-resource/angular-resource.min.js',
+            '<%= grunt.config.app %>/bower_components/angular-animate/angular-animate.min.js',
+            '<%= grunt.config.app %>/bower_components/angular-sanitize/angular-sanitize.min.js',
+            '<%= grunt.config.app %>/bower_components/angular-touch/angular-touch.min.js'
+          ]
+        }
+      }
   });
-
 };
